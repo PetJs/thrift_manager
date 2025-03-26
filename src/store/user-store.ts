96df0163f2ba650186a142784a43040c99fc74e0
Susu-spec <suleimansuwaibat@gmail.com>
2025-03-26 04:58:54 +0100

    feat: Refactors Role variables

--- src/store/user-store.ts
+++ src/store/user-store.ts
@@ -7,13 +7,13 @@ import { persist } from "zustand/middleware";
 
 export interface UserStore {
   user: User | null;
-  activeRole: UserRole | null;
+  currentRole: UserRole | null;
   accessToken: string | null;
   refreshToken: string | null;
   setUser: (data: { user: User }) => void;
   updateUser: (user: User) => void;
   setTokens: (accessToken: string, refreshToken: string) => void;
-  setActiveRole: (role: UserRole | null) => void;
+  setCurrentRole: (role: UserRole | null) => void;
   reset: () => void;
 }
 
@@ -26,7 +26,7 @@ const useUserStore = create<UserStore>(
   (persist as MyPersist)(
     (set) => ({
       user: null,
-      activeRole: null,
+      currentRole: null,
       userId: null,
       accessToken: null,
       refreshToken: null,
@@ -34,16 +34,16 @@ const useUserStore = create<UserStore>(
       setUser: ({ user }) =>
         set({
           user,
-          activeRole: user.role as UserRole,
+          currentRole: user.role as UserRole,
         }),
       updateUser: (user) => set({ user }),
       setTokens: (accessToken, refreshToken) =>
         set({ accessToken, refreshToken }),
-      setActiveRole: (role) => set({ activeRole: role }),
+      setCurrentRole: (role) => set({ currentRole: role }),
       reset: () =>
         set({
           user: null,
-          activeRole: null,
+          currentRole: null,
           accessToken: null,
           refreshToken: null,
         }),
