de7e2c68fb3b09097d78237266235e507eda2bbd
PetJs <peterfagoroye76@gmail.com>
2025-03-26 17:41:02 +0100

    style: :lipstick: Modifies UI

--- src/layouts/settings-layout.tsx
+++ src/layouts/settings-layout.tsx
@@ -7,15 +7,15 @@ export default function SettingLayout() {
     const settingItems = [
         { label: "My Profile", path: "/settings/profile" },
         { label: "Payment and Bank Details", path: "/settings/payment-details" },
-        { label: "Rotation Schedule", path: "/schedule" },
-        { label: "Settings", path: "/pro" },
+        { label: "Notification Prefrences", path: "/settings/notification" },
+        { label: "Security Settings", path: "/settings/security" },
     ]
       
     return (
         <main className="flex relative">
-            <div className="flex-1  relative">
+            <div className="flex-1 relative">
                     <div>
-                        <h2>Settings</h2>
+                        <h2 className="text-[22px] font-bold">Settings</h2>
                         <SettingNav items={settingItems}/>
                     </div>
                     <Outlet />
