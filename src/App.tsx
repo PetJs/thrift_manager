09ca9e66f6d61f338e7d2f64be7c0f7a950a5329
Susu-spec <suleimansuwaibat@gmail.com>
2025-03-26 03:58:57 +0100

    feat: Refactors `App.tsx` file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom';
-import SignIn from './user/auth/signInPage';
-import OTPVerification from './user/auth/otpPage';
-import ResetPassword from './user/auth/resetPasswordPage';
-import Dashboard from './user/dashboard';
-import ContributionPage from './user/contributionPage';
-import SchedulePage from './user/schedulePage';
+import { Routes } from './routes'
+
 
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/sign-in" element={<SignIn/> } />
-      <Route path="/otp-verification" element={<OTPVerification/>} />
-      <Route path="/reset-password" element={<ResetPassword/>} />
-      <Route path="/" element={<Dashboard/>}/>
-      <Route path="/contribution-page" element={<ContributionPage/>}/>
-      <Route path="/schedule-page" element={<SchedulePage/>}/>
-    </Routes>
+      <Routes />
     </>
   )
 }
