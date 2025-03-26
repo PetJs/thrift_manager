a5a63936aca8aa5fbd718c58d9489a5a51b9dc52
PetJs <peterfagoroye76@gmail.com>
2025-03-26 12:23:08 +0100

    feat: :sparkles: Creates Settings Nav bar component

---
+++ src/components/ui/settings-nav.tsx
@@ -0,0 +1,28 @@
+import { SettingNavProps } from "@/lib/types";
+import { NavLink } from "react-router-dom";
+
+
+const SettingNav: React.FC<SettingNavProps> = ({ items }) => {
+
+  return (
+    <nav className="flex mt-6">
+        <ul className="flex gap-6 px-4 py-3 ">
+        {items.map(({ label, path }) => (
+        <li key={path}>
+            <NavLink
+            to={path}
+            className={({ isActive }) =>
+                isActive ? "border-b-2 border-blue-700 py-2 "
+                    : "text-gray-700 hover:border-b-2 hover:border-blue-700 py-1"
+            }
+            >
+            {label}
+            </NavLink>
+        </li>
+        ))}
+        </ul>
+    </nav>
+  );
+};
+
+export default SettingNav;
