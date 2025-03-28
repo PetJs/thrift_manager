4b3584757ae6c89183e8cbf02929aa4d69d30306
PetJs <peterfagoroye76@gmail.com>
2025-03-28 23:14:37 +0100

    style: :lipstick: Updtaes crad ui

--- src/components/ui/paymentNotifCard.tsx
+++ src/components/ui/paymentNotifCard.tsx
@@ -9,10 +9,12 @@ const PaymentNotifCard: React.FC<PaymentNotifCardProps> = ({ recipient, amount, 
             <div className="text-[13px] font-medium text-gray-500 mb-2">Next Recipient:</div>
             <div className="text-[18px] font-semibold text-black">{recipient}</div>
         </div>
-        <div>
-            <div className="text-[13px] font-medium text-gray-500 mb-2">Amount:</div>
-            <div className="text-[18px] font-semibold text-black">{amount}</div>
-        </div>
+        {amount && 
+            <div>
+                <div className="text-[13px] font-medium text-gray-500 mb-2">Amount:</div>
+                <div className="text-[18px] font-semibold text-black">{amount}</div>
+            </div>
+        }
         <div>
             <div className="text-[13px] font-medium text-gray-500 mb-2">Date:</div>
             <div className="text-[18px] font-semibold text-black">{date}</div>
