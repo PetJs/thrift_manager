4f9b7714582bebe7e257f13bf3292733bc9823f0
PetJs <peterfagoroye76@gmail.com>
2025-03-28 16:18:45 +0100

    refactor: :art: Update card to handle input from Admin dashboard

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -5,25 +5,29 @@ import { CardProps } from "@/lib/types";
 
 const Card: React.FC<CardProps> = ({
   icon,
+  header,
   amount,
   description,
   tag,
   className,
   actionText,
+  current,
+  next,
   onActionClick,
 }) => {
   return (
     <div className="flex flex-col w-[257px] h-[135px] bg-white shadow-lg rounded-xl p-4">
         <div className="flex items-center justify-between mb-4">
-            <div className="flex items-center">
-            <div className="w-[35px] h-[35px] flex items-center justify-center bg-blue-100 rounded-full">
-                {/* Ensure icon is passed as ReactNode */}
-                {typeof icon === "string" ? (
-                <img src={icon} alt="icon" className="w-6 h-6" />
-                ) : (
-                icon
-                )}
-            </div>
+            <div className="flex items-center gap-2">
+                <div className="w-[35px] h-[35px] flex items-center justify-center bg-blue-100 rounded-full">
+                    {/* Ensure icon is passed as ReactNode */}
+                    {typeof icon === "string" ? (
+                    <img src={icon} alt="icon" className="w-6 h-6" />
+                    ) : (
+                    icon
+                    )}
+                </div>
+                <h1 className="text-[15px]">{header}</h1>
             </div>
             {tag ? (
             <div
@@ -42,6 +46,10 @@ const Card: React.FC<CardProps> = ({
         </div>
         <h1 className="text-[20px] font-semibold text-black mt-2">{amount}</h1>
         <p className="text-gray-500 text-[13px] mt-1">{description}</p>
+        <div className=" gap-4">
+            <p className="text-gray-500 text-[13px]">{current}</p>
+            <p className="text-gray-500 text-[13px]">{next}</p>
+        </div>
     </div>
   );
 };
